b0d8907b2f75cabb6dbb8c9b7728f7abc5f262d2
alya <ellliotros@gmail.com>
2025-06-13 18:14:30 +0200

    Delete unused keyReleaseEvent

--- GameManager.h
+++ GameManager.h
@@ -3,22 +3,17 @@
 #include <QGraphicsView>
 #include "PlayerItem.h"
 
-
 class GameManager : public QGraphicsView {
 public:
     explicit GameManager(QGraphicsScene *scene);
-
-
     std::shared_ptr<PlayerItem> getLeftPlayer(){ return m_leftPlayer; }
     std::shared_ptr<PlayerItem>  getRightPlayer(){ return m_rightPlayer; }
     void keyPressEvent(QKeyEvent* event) override;
-    void keyReleaseEvent(QKeyEvent* event) override;
 
 private:
     std::shared_ptr<PlayerItem> m_rightPlayer = nullptr;
     std::shared_ptr<PlayerItem> m_leftPlayer = nullptr;
 };
-
 
 
 #endif //GAMEMANAGER_H
