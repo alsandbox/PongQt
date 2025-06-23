a785cd8bb6efc2cb50213d954806a5a02c53b8ec
alya <ellliotros@gmail.com>
2025-06-23 16:01:59 +0200

    Fix logic in updateFrame function

--- GameManager.cpp
+++ GameManager.cpp
@@ -4,6 +4,9 @@ GameManager::GameManager(QGraphicsScene* scene) {
     m_leftPlayer = std::make_shared<PlayerItem>(Qt::Key_W, Qt::Key_S, scene);
     m_rightPlayer = std::make_shared<PlayerItem>(Qt::Key_Up, Qt::Key_Down, scene);
     m_timer = new QTimer(this);
+    connect(m_timer, &QTimer::timeout, this, [this]() {
+    if (m_updateFunc) m_updateFunc();
+});
 }
 
 void GameManager::keyPressEvent(QKeyEvent* event) {
@@ -22,10 +25,7 @@ void GameManager::keyReleaseEvent(QKeyEvent *event) {
 
 void GameManager::updateFrame(const std::function<void()> &func) {
     m_updateFunc = func;
-    if (m_timer && !m_timer->isActive()) {
-        connect(m_timer, &QTimer::timeout, this, [this]() {
-            if (m_updateFunc) m_updateFunc();
-        });
+    if (!m_timer->isActive()) {
         constexpr int frameIntervalMs = 16;
         m_timer->start(frameIntervalMs);
     }
