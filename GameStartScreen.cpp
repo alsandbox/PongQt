5167fa30cc4644a317bbddf48330f93d8a4e05d9
elliot <ellliotros@gmail.com>
2025-08-08 17:41:23 +0200

    Create Start screen

---
+++ GameStartScreen.cpp
@@ -0,0 +1,58 @@
+﻿#include "GameStartScreen.h"
+#include "fonts/FontConfig.h"
+
+GameStartScreen::GameStartScreen(QGraphicsScene *scene)
+    : ScreenBase(scene) {
+}
+
+void GameStartScreen::addButtons() {
+    if (!m_background) {
+        m_background = m_scene->addRect(
+            m_scene->sceneRect(),
+            QPen(Qt::NoPen),
+            QBrush(Qt::black)
+        );
+        m_background->setZValue(100);
+    }
+
+    if (!m_startProxyButton) {
+        createSpecifiedButton(m_startButton, m_startProxyButton, "Start");
+        m_startProxyButton->setZValue(101);
+
+        connect(m_startButton, &QPushButton::pressed,
+                this, &GameStartScreen::onStartButtonPressed);
+    }
+
+    createExitButton();
+    if (m_exitProxyButton) {
+        m_exitProxyButton->setZValue(101);
+        connect(m_exitButton, &QPushButton::pressed,
+                this, &GameStartScreen::onExitButtonPressed);
+    }
+}
+
+void GameStartScreen::removeElements() {
+    if (m_startProxyButton) {
+        m_startProxyButton->hide();
+        m_startButton->hide();
+    }
+
+    if (m_exitProxyButton) {
+        m_exitProxyButton->hide();
+        m_exitButton->hide();
+    }
+
+    if (m_background) {
+        m_background->hide();
+    }
+}
+
+void GameStartScreen::showEvent(QShowEvent *event) {
+    ScreenBase::showEvent(event);
+    addButtons();
+}
+
+void GameStartScreen::onStartButtonPressed() {
+    emit startButtonPressed();
+    removeElements();
+}
---
+++ GameStartScreen.h
@@ -0,0 +1,26 @@
+﻿#ifndef GAMESTARTSCREEN_H
+#define GAMESTARTSCREEN_H
+#include "ScreenBase.h"
+#include <QGraphicsProxyWidget>
+
+class GameStartScreen : public ScreenBase {
+    Q_OBJECT
+public:
+    explicit GameStartScreen(QGraphicsScene *scene);
+    void addButtons() override;
+    void removeElements() override;
+    void showEvent(QShowEvent* event) override;
+
+    signals:
+        void startButtonPressed();
+
+private:
+    void onStartButtonPressed();
+
+    QGraphicsRectItem *m_background = nullptr;
+    QGraphicsProxyWidget *m_startProxyButton = nullptr;
+    QPushButton *m_startButton = nullptr;
+};
+
+
+#endif //GAMESTARTSCREEN_H
