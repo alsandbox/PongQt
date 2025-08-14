c99e72b326d435e5242de2e967619ef6cb017cd7
elliot <ellliotros@gmail.com>
2025-08-14 14:18:17 +0200

    Refactor displayLine() to handle screen resizing

--- renderers/LineRenderer.cpp
+++ renderers/LineRenderer.cpp
@@ -7,14 +7,18 @@ LineRenderer::LineRenderer(QGraphicsScene *scene) : m_scene(scene) {
 
 void LineRenderer::displayLine(const QRectF &rect) {
     constexpr qreal half = 2;
+    constexpr int scalingFactor = 150;
+    const int penWidth = rect.width() / scalingFactor;
+
+    QPen pen(Qt::white);
+    pen.setWidth(penWidth);
+    pen.setStyle(Qt::DashLine);
+
     if (!lineItem) {
-        QPen pen(Qt::white);
-        constexpr qreal penWidth = 2;
-        pen.setWidth(penWidth);
-        pen.setStyle(Qt::DashLine);
         lineItem = m_scene->addLine(QLineF(rect.width() / half, 0, rect.width() / half, rect.height()), pen);
     } else {
         lineItem->setLine(QLineF(rect.width() / half, 0, rect.width() / half, rect.height()));
+        lineItem->setPen(pen);
     }
 }
 
