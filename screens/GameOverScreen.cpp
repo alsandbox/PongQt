824b8b22aa1f32161369ab24c10ed04e1e4da3a5
elliot <ellliotros@gmail.com>
2025-08-14 14:06:27 +0200

    Move into screens folder

---
+++ screens/GameOverScreen.cpp
@@ -0,0 +1,48 @@
+﻿#include "GameOverScreen.h"
+#include <QGraphicsProxyWidget>
+#include <QPushButton>
+
+GameOverScreen::GameOverScreen(QGraphicsScene *scene, LineRenderer* lineRenderer,
+                              BallRenderer* ballRenderer) : ScreenBase(scene),
+    m_lineRenderer(lineRenderer),
+    m_ballRenderer(ballRenderer) {
+}
+
+void GameOverScreen::removeElements() {
+    m_lineRenderer->hideLine();
+    m_ballRenderer->hideBall();
+}
+
+void GameOverScreen::addButtons()  {
+    ScreenBase::addButtons();
+
+    if (!m_restartProxyButton) {
+        createSpecifiedButton(m_restartButton, m_restartProxyButton, "Restart");
+        m_buttons.append(m_restartButton);
+        m_proxyButtons.append(m_restartProxyButton);
+        m_layout->addItem(m_restartProxyButton);
+        connect(m_restartButton, &QPushButton::pressed, this, &GameOverScreen::onRestartButtonPressed);
+    }
+
+    createExitButton();
+    if (m_exitProxyButton) {
+        m_buttons.append(m_exitButton);
+        m_proxyButtons.append(m_exitProxyButton);
+        m_layout->addItem(m_exitProxyButton);
+        connect(m_exitButton, &QPushButton::pressed, this, &GameOverScreen::onExitButtonPressed);
+    }
+
+    m_layout->setAlignment(m_restartProxyButton, Qt::AlignHCenter);
+    m_layout->setAlignment(m_exitProxyButton, Qt::AlignHCenter);
+    m_buttonContainer->adjustSize();
+
+    updateLayout(m_scene->sceneRect().size().toSize());
+}
+
+void GameOverScreen::onRestartButtonPressed() {
+
+}
+
+void GameOverScreen::updateLayout(const QSize &size) {
+    ScreenBase::updateLayout(size);
+}
---
+++ screens/GameStartScreen.cpp
@@ -0,0 +1,81 @@
+﻿#include "GameStartScreen.h"
+
+#include "../fonts/FontConfig.h"
+
+GameStartScreen::GameStartScreen(QGraphicsScene *scene)
+    : ScreenBase(scene) {
+}
+
+void GameStartScreen::addBackground() {
+    const QRectF sceneRect = m_scene->sceneRect();
+    constexpr qreal margin = 20;
+    const QRectF expandedRect(sceneRect.left() - margin, sceneRect.top() - margin,
+                              sceneRect.width() + m_half * margin, sceneRect.height() + m_half * margin);
+
+    if (!m_background) {
+        m_background = m_scene->addRect(expandedRect, QPen(Qt::NoPen), QBrush(Qt::black));
+        m_background->setZValue(100);
+    } else {
+        m_background->setRect(expandedRect);
+    }
+}
+
+void GameStartScreen::addButtons() {
+    ScreenBase::addButtons();
+    addBackground();
+    if (!m_startProxyButton) {
+        createSpecifiedButton(m_startButton, m_startProxyButton, "Start");
+        m_buttons.append(m_startButton);
+        m_proxyButtons.append(m_startProxyButton);
+        m_layout->addItem(m_startProxyButton);
+        connect(m_startButton, &QPushButton::pressed, this, &GameStartScreen::onStartButtonPressed);
+    }
+
+    createExitButton();
+    if (m_exitProxyButton) {
+        m_buttons.append(m_exitButton);
+        m_proxyButtons.append(m_exitProxyButton);
+        m_layout->addItem(m_exitProxyButton);
+        connect(m_exitButton, &QPushButton::pressed, this, &GameStartScreen::onExitButtonPressed);
+    }
+
+    m_layout->setAlignment(m_startProxyButton, Qt::AlignHCenter);
+    m_layout->setAlignment(m_exitProxyButton, Qt::AlignHCenter);
+    m_buttonContainer->adjustSize();
+}
+
+void GameStartScreen::updateLayout(const QSize &size) {
+    ScreenBase::updateLayout(size);
+    if (m_background) {
+        m_background->setRect(m_scene->sceneRect());
+    } else {
+        addBackground();
+    }
+}
+
+void GameStartScreen::removeElements() {
+    if (m_startProxyButton) {
+        m_startProxyButton->hide();
+        m_startButton->hide();
+    }
+
+    if (m_exitProxyButton) {
+        m_exitProxyButton->hide();
+        m_exitButton->hide();
+    }
+
+    if (m_background) {
+        m_background->hide();
+    }
+}
+
+void GameStartScreen::showEvent(QShowEvent *event) {
+    ScreenBase::showEvent(event);
+    addButtons();
+    updateLayout(m_scene->sceneRect().size().toSize());
+}
+
+void GameStartScreen::onStartButtonPressed() {
+    emit startButtonPressed();
+    removeElements();
+}
